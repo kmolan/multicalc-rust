c6e3679877af183ff3c8a72f687a7b3dbcd5900a
Anmol Kathail <anmolkathail@gmail.com>
2025-10-13 09:44:43 -0500

    auto format at build

--- build.rs
+++ build.rs
@@ -1,6 +1,11 @@
 use std::process::Command;
 
 fn main() {
+    autogenerate_gaussian_tables();
+    run_cargo_fmt();
+}
+
+fn autogenerate_gaussian_tables() {
     let script_path = "scripts/build_gaussian_integration_tables.py";
 
     let status = Command::new("python3")
@@ -11,5 +16,15 @@ fn main() {
     if !status.success() {
         panic!("Python script failed with status: {:?}", status);
     }
-    
+}
+
+fn run_cargo_fmt() {
+    let status = Command::new("cargo")
+        .args(&["fmt"])
+        .status()
+        .expect("Failed to run cargo fmt");
+
+    if !status.success() {
+        panic!("cargo fmt failed with status: {:?}", status);
+    }
 }
