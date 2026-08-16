fc6dcfdc91cf0d2fbe7fb6f2c758f66dbac14b3a
Anmol Kathail <anmolkathail@gmail.com>
2026-08-15 21:37:37 -0500

    fix pipe

--- crates/multicalc-robot-model/src/lib.rs
+++ crates/multicalc-robot-model/src/lib.rs
@@ -232,13 +232,13 @@ assert_eq!(model.ignored(), ["actuator".to_owned()]);
     pub(crate) fn reject_mimic_joints(&self, slots: &[usize]) -> Result<(), ModelError> {
         for &index in slots {
             let body = &self.bodies[index];
-            if let Some(description) = &body.joint
-                && let Some(mimic) = &description.mimic
-            {
-                return Err(ModelError::MimicJointInTree {
-                    joint: description.name.clone(),
-                    follows: mimic.joint.clone(),
-                });
+            if let Some(description) = &body.joint {
+                if let Some(mimic) = &description.mimic {
+                    return Err(ModelError::MimicJointInTree {
+                        joint: description.name.clone(),
+                        follows: mimic.joint.clone(),
+                    });
+                }
             }
         }
         Ok(())
